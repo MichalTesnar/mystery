958739a6a46712532b80587ee0c04b16cee51fef
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-10-09 13:03:38 +0200

    Adding figures for Matias

--- Broken Uncertain Models for Matias/ensembles-test.py
+++ Broken Uncertain Models for Matias/ensembles-test.py
@@ -66,10 +66,6 @@ def pred_ensembles(model, x_train, y_train, domain):
 if __name__ == "__main__":
     # data
     x_train = np.linspace(-4.0, 4.0, num=2*SAMPLE_RATE)
-    # train_indices = np.arange()
-    # train_indices = np.random.choice(2*SAMPLE_RATE, SAMPLE_RATE, replace=False)
-    # train_indices = np.append(np.arange(0, 10), 19)
-    # train_indices = np.append(np.arange(0, 50), np.arange(150,199))
     train_indices = np.append(np.arange(0, 90), np.arange(190,199))
     np.random.shuffle(train_indices)
     x_train = x_train[train_indices]
@@ -89,14 +85,11 @@ if __name__ == "__main__":
     # define model to be retrained later
     # iteratively retrain on new data
     current_x_train, current_y_train = x_train, y_train
-    # y_pred_mean, y_pred_std, current_model = retrain_dropout_model(current_model, current_x_train, current_y_train, domain)
     model = train_ensemble_model()
     y_pred_mean, y_pred_std = pred_ensembles(model, current_x_train, current_y_train, domain)
-    print(y_pred_std)
     # compute metrics        
     score = gaussian_interval_score(domain_y, y_pred_mean, y_pred_std)
     calib_err = regressor_calibration_error(y_pred_mean, domain_y, y_pred_std)
-    # print(f"score: {score:.2f} calib_err: {calib_err:.2f}")
     # plot data
     y_pred_mean = y_pred_mean.reshape((-1,))
     y_pred_std = y_pred_std.reshape((-1,))
@@ -104,7 +97,6 @@ if __name__ == "__main__":
     y_pred_down_1 = y_pred_mean - y_pred_std
     ax.plot(current_x_train, current_y_train, '.', color=(0.9, 0, 0, 1), markersize=5, label="Trainng Set")
     ax.plot(domain, domain_y, '.', color=(0, 0.9, 0, 1), markersize=3, label="Target Function")
-    # ax.plot(domain, points, '.', color=(0.9, 0.9, 0.9, 1), markersize=5)
     ax.fill_between(domain.ravel(), y_pred_down_1, y_pred_up_1,  color=(0, 0.5, 0.9, 1))
     ax.plot(domain.ravel(), y_pred_mean, '.', color=(1, 1, 1, 0.8), markersize=0.2)
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
