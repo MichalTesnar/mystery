2d06e16e28917b7829c67de0a867c196454b42fd
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-11-07 10:54:29 +0100

    Visualize dataset

---
+++ dataset_exploration/input_features.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the data
+csv_file_path = "../dagon_dataset.csv"
+df = pd.read_csv(csv_file_path, skiprows=1, nrows=2000)
+u = df.iloc[:, 0]
+v = df.iloc[:, 1]
+r = df.iloc[:, 2]
+th1 = df.iloc[:, 3]
+th2 = df.iloc[:, 4]
+th3 = df.iloc[:, 5]
+x = np.arange(0, len(th1))
+
+# Create a figure and three subplots arranged vertically
+fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(6, 1, figsize=(8, 12), sharex=True)
+fig.suptitle('Input Features of AUV Dagon Dataset', fontsize=20)
+
+# Plot data
+ax1.plot(x, u, label=r'$u$ $(m/s)$')
+ax2.plot(x, v, label=r'$v$ $(m/s)$')
+ax3.plot(x, r, label=r'$r$ $(rd/s)$')
+ax4.plot(x, th1, label=r'$n_1$ $(rps)$')
+ax5.plot(x, th2, label=r'$n_2$ $(rps)$')
+ax6.plot(x, th3, label=r'$n_3$ $(rps)$')
+ax1.legend(loc='upper left', fontsize="20")
+ax2.legend(loc='upper left', fontsize="20")
+ax3.legend(loc='upper left', fontsize="20")
+ax4.legend(loc='upper left', fontsize="20")
+ax5.legend(loc='upper left', fontsize="20")
+ax6.legend(loc='upper left', fontsize="20")
+
+# Show the plots
+plt.tight_layout()
+plt.savefig("input_features.png")
+plt.show()
---
+++ dataset_exploration/targets.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the data
+csv_file_path = "../dagon_dataset.csv"
+df = pd.read_csv(csv_file_path, skiprows=1, nrows=2000)
+u_dot = df.iloc[:, 6]
+v_dot = df.iloc[:, 7]
+r_dot = df.iloc[:, 8]
+x = np.arange(0, len(u_dot))
+
+# Create a figure and three subplots arranged vertically
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 6.4), sharex=True)
+fig.suptitle('Targets of AUV Dagon Dataset', fontsize=20)
+
+# Plot data
+ax1.plot(x, u_dot, label=r'$\dot{u}$ $(m/s^2)$')
+ax2.plot(x, v_dot, label=r'$\dot{v}$ $(m/s^2)$')
+ax3.plot(x, r_dot, label=r'$\dot{r}$ $(rd/s^2)$')
+ax1.legend(loc='upper left', fontsize="20")
+ax2.legend(loc='upper left', fontsize="20")
+ax3.legend(loc='upper left', fontsize="20")
+
+# Show the plots
+plt.tight_layout()
+plt.savefig("targets.png")
+plt.show()
