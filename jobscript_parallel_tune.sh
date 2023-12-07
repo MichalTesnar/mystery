6e0412a7dcbcc098f1aa36cb12089667f346ca61
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-12-07 22:21:07 +0100

    EPIC TUNER

---
+++ jobscript_parallel_tune.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=50
+#SBATCH --partition=regular
+
+module load Python/3.9.6-GCCcore-11.2.0
+
+source $HOME/venvs/mystery/bin/activate
+
+
+# python3 online_learning_tuning.py $1 # run the script with different arguments
+bash main_worker.sh &
+bash other_worker.sh 0 &
+bash other_worker.sh 1 &
+bash other_worker.sh 2 &
+bash other_worker.sh 3 &
+
+
+module load git
+git config --global user.email "michal.tesnar007@gmail.com"
+git config --global user.name "MichalTesnar"
+git add --a
+git commit -m "Pushing online_learning_tuning.py results"
+git push
+
+deactivate
--- worker1.sh
+++
@@ -1,4 +0,0 @@
-export KERASTUNER_TUNER_ID="tuner1"
-export KERASTUNER_ORACLE_IP="127.0.0.1"
-export KERASTUNER_ORACLE_PORT="8000"
-python3 online_learning_tuning.py
