9154790881eac07275d8b6eb4460e330ccc2e421
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-12-18 20:33:48 +0100

    Fly bird, fly

--- jobscript_parallel_tune.sh
+++ jobscript_parallel_tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=32
 #SBATCH --partition=regular
 
 module load Python/3.9.6-GCCcore-11.2.0
@@ -14,7 +14,19 @@ bash main_worker.sh $1 &
 bash other_worker.sh 0 $1 &
 bash other_worker.sh 1 $1 &
 bash other_worker.sh 2 $1 &
-bash other_worker.sh 3 $1
+bash other_worker.sh 3 $1 &
+bash other_worker.sh 4 $1 &
+bash other_worker.sh 5 $1 &
+bash other_worker.sh 6 $1 &
+bash other_worker.sh 7 $1 &
+bash other_worker.sh 8 $1 &
+bash other_worker.sh 9 $1 &
+bash other_worker.sh 10 $1 &
+bash other_worker.sh 11 $1 &
+bash other_worker.sh 12 $1 &
+bash other_worker.sh 13 $1 &
+bash other_worker.sh 14 $1 &
+bash other_worker.sh 15 $1
 
 
 module load git
