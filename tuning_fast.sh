c535c987317d573774f7403551b88c38478b908d
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-12-22 13:53:36 +0100

    I am hopeful

--- tuning_fast.sh
+++ tuning_fast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=40
 #SBATCH --partition=regular
 
 module load Python/3.9.6-GCCcore-11.2.0
