a2cfeea0ba0d6c35a996d19c279cd19a83280077
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-12-22 14:01:41 +0100

    Adding ports as variable

--- other_worker.sh
+++ other_worker.sh
@@ -1,4 +1,4 @@
 export KERASTUNER_TUNER_ID="tuner $1"
 export KERASTUNER_ORACLE_IP="127.0.0.1"
-export KERASTUNER_ORACLE_PORT="8000"
+export KERASTUNER_ORACLE_PORT="$3"
 python3 online_learning_tuning.py $2
--- tuning_fast.sh
+++ tuning_fast.sh
@@ -8,67 +8,68 @@ module load Python/3.9.6-GCCcore-11.2.0
 
 source $HOME/venvs/mystery/bin/activate
 
-bash main_worker.sh $1 &
-bash other_worker.sh 0 $1 &
-bash other_worker.sh 1 $1 &
-bash other_worker.sh 2 $1 &
-bash other_worker.sh 3 $1 &
-bash other_worker.sh 4 $1 &
-bash other_worker.sh 5 $1 &
-bash other_worker.sh 6 $1 &
-bash other_worker.sh 7 $1 &
-bash other_worker.sh 8 $1 &
-bash other_worker.sh 9 $1 &
-bash other_worker.sh 10 $1 &
-bash other_worker.sh 11 $1 &
-bash other_worker.sh 12 $1 &
-bash other_worker.sh 13 $1 &
-bash other_worker.sh 14 $1 &
-bash other_worker.sh 15 $1 &
-bash other_worker.sh 16 $1 &
-bash other_worker.sh 17 $1 &
-bash other_worker.sh 18 $1 &
-bash other_worker.sh 19 $1 &
-bash other_worker.sh 20 $1 &
-bash other_worker.sh 21 $1 &
-bash other_worker.sh 22 $1 &
-bash other_worker.sh 23 $1 &
-bash other_worker.sh 24 $1 &
-bash other_worker.sh 25 $1 &
-bash other_worker.sh 26 $1 &
-bash other_worker.sh 27 $1 &
-bash other_worker.sh 28 $1 &
-bash other_worker.sh 29 $1 &
-bash other_worker.sh 30 $1 &
-bash other_worker.sh 31 $1 &
-bash other_worker.sh 32 $1 &
-bash other_worker.sh 33 $1 &
-bash other_worker.sh 34 $1 &
-bash other_worker.sh 35 $1 &
-bash other_worker.sh 36 $1 &
-bash other_worker.sh 37 $1 &
-bash other_worker.sh 38 $1 &
-bash other_worker.sh 39 $1 &
-bash other_worker.sh 40 $1 &
-bash other_worker.sh 41 $1 &
-bash other_worker.sh 42 $1 &
-bash other_worker.sh 43 $1 &
-bash other_worker.sh 44 $1 &
-bash other_worker.sh 45 $1 &
-bash other_worker.sh 46 $1 &
-bash other_worker.sh 47 $1 &
-bash other_worker.sh 48 $1 &
-bash other_worker.sh 49 $1 &
-bash other_worker.sh 50 $1 &
-bash other_worker.sh 51 $1 &
-bash other_worker.sh 52 $1 &
-bash other_worker.sh 53 $1 &
-bash other_worker.sh 54 $1 &
-bash other_worker.sh 55 $1 &
-bash other_worker.sh 56 $1 &
-bash other_worker.sh 57 $1 &
-bash other_worker.sh 58 $1 &
-bash other_worker.sh 59 $1
+# type of runtime / port
+bash main_worker.sh $1 $2 &
+bash other_worker.sh 0 $1 $2 &
+bash other_worker.sh 1 $1 $2 &
+bash other_worker.sh 2 $1 $2 &
+bash other_worker.sh 3 $1 $2 &
+bash other_worker.sh 4 $1 $2 &
+bash other_worker.sh 5 $1 $2 &
+bash other_worker.sh 6 $1 $2 &
+bash other_worker.sh 7 $1 $2 &
+bash other_worker.sh 8 $1 $2 &
+bash other_worker.sh 9 $1 $2 &
+bash other_worker.sh 10 $1 $2 &
+bash other_worker.sh 11 $1 $2 &
+bash other_worker.sh 12 $1 $2 &
+bash other_worker.sh 13 $1 $2 &
+bash other_worker.sh 14 $1 $2 &
+bash other_worker.sh 15 $1 $2 &
+bash other_worker.sh 16 $1 $2 &
+bash other_worker.sh 17 $1 $2 &
+bash other_worker.sh 18 $1 $2 &
+bash other_worker.sh 19 $1 $2 &
+bash other_worker.sh 20 $1 $2 &
+bash other_worker.sh 21 $1 $2 &
+bash other_worker.sh 22 $1 $2 &
+bash other_worker.sh 23 $1 $2 &
+bash other_worker.sh 24 $1 $2 &
+bash other_worker.sh 25 $1 $2 &
+bash other_worker.sh 26 $1 $2 &
+bash other_worker.sh 27 $1 $2 &
+bash other_worker.sh 28 $1 $2 &
+bash other_worker.sh 29 $1 $2 &
+bash other_worker.sh 30 $1 $2 &
+bash other_worker.sh 31 $1 $2 &
+bash other_worker.sh 32 $1 $2 &
+bash other_worker.sh 33 $1 $2 &
+bash other_worker.sh 34 $1 $2 &
+bash other_worker.sh 35 $1 $2 &
+bash other_worker.sh 36 $1 $2 &
+bash other_worker.sh 37 $1 $2 &
+bash other_worker.sh 38 $1 $2 &
+bash other_worker.sh 39 $1 $2 &
+bash other_worker.sh 40 $1 $2 &
+bash other_worker.sh 41 $1 $2 &
+bash other_worker.sh 42 $1 $2 &
+bash other_worker.sh 43 $1 $2 &
+bash other_worker.sh 44 $1 $2 &
+bash other_worker.sh 45 $1 $2 &
+bash other_worker.sh 46 $1 $2 &
+bash other_worker.sh 47 $1 $2 &
+bash other_worker.sh 48 $1 $2 &
+bash other_worker.sh 49 $1 $2 &
+bash other_worker.sh 50 $1 $2 &
+bash other_worker.sh 51 $1 $2 &
+bash other_worker.sh 52 $1 $2 &
+bash other_worker.sh 53 $1 $2 &
+bash other_worker.sh 54 $1 $2 &
+bash other_worker.sh 55 $1 $2 &
+bash other_worker.sh 56 $1 $2 &
+bash other_worker.sh 57 $1 $2 &
+bash other_worker.sh 58 $1 $2 &
+bash other_worker.sh 59 $1 $2 
 
 module load git
 git config --global user.email "michal.tesnar007@gmail.com"
