5f965cde8f1fa259949f45e098d67e6afe12c1e8
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-12-26 10:56:29 +0100

    LOL

--- online_learning_tuning.py
+++ online_learning_tuning.py
@@ -15,9 +15,9 @@ from src.model import AIOModelTuning, AIOModel
 import copy
 import sys
 
-import tensorflow as tf
+# import tensorflow as tf
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 DATASET_TYPE = "Dagon"
 EXP_TYPE = "Offline"
