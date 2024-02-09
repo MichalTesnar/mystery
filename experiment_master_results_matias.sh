4e3cf53bc43c449ffd4e88983306b478ef190d10
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2024-02-09 10:09:35 +0100

    Actually not

--- experiment_master_results_matias.sh
+++ experiment_master_results_matias.sh
@@ -1,10 +1,17 @@
 source $HOME/venvs/mystery/bin/activate
 
-nohup python3 main_auto.py "FIFO" > log_FIFO.out &
-nohup python3 main_auto.py "FIRO" > log_FIRO.out &
-nohup python3 main_auto.py "RIRO" > log_RIRO.out &
-nohup python3 main_auto.py "THRESHOLD" > log_THRESHOLD.out &
-nohup python3 main_auto.py "GREEDY" > log_GREEDY.out &
-nohup python3 main_auto.py "THRESHOLD_GREEDY" > log_THRESHOLD_GREEDY.out &
+nohup python3 main_auto.py "FIFO" &
+nohup python3 main_auto.py "FIRO" &
+nohup python3 main_auto.py "RIRO" &
+nohup python3 main_auto.py "THRESHOLD" &
+nohup python3 main_auto.py "GREEDY" &
+nohup python3 main_auto.py "THRESHOLD_GREEDY" &
+
+# nohup python3 main_auto.py "FIFO" > log_FIFO.out &
+# nohup python3 main_auto.py "FIRO" > log_FIRO.out &
+# nohup python3 main_auto.py "RIRO" > log_RIRO.out &
+# nohup python3 main_auto.py "THRESHOLD" > log_THRESHOLD.out &
+# nohup python3 main_auto.py "GREEDY" > log_GREEDY.out &
+# nohup python3 main_auto.py "THRESHOLD_GREEDY" > log_THRESHOLD_GREEDY.out &
 
 wait
