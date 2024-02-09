5e2504c27b808792693c6ab2193ac4ebc24cf02a
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2024-02-09 10:24:17 +0100

    Test if it works

--- analysis/flipout.py
+++ analysis/flipout.py
@@ -61,7 +61,7 @@ def line_color(st):
 prefix = ""
 # DIRECTORIES THAT NEED TO BE CONSIDERED
 dir_names = [
-    "flipout test (2)",
+    "Flipout Full data fix THRESHOLD  tuned (0)",
 ]
 
 # IDENTIFIER TO PUT ON THE PLOT
@@ -99,6 +99,7 @@ for j, dir_name in enumerate(dir_names):
             continue
 
         y = metrics_results[metric]
+        print(len(y))
         x = np.arange(0, len(y))
         if metric == "MSE":
             y = np.minimum(0.035, y)
